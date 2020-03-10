849caaa2ac3de27f1be0fa614dd9a4f3ea8c7137
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-10 18:33:33 +0900

    /hello /hello/otherにアクセスした際にページを表示する処理を追加した。
    htmlの記載はコントローラ直張

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index() {
+        return <<<EOF
+        <html>
+        <body>
+        <p>これはHelloコントローラのIndexアクションです</p>
+        </body>
+        </html>
+        EOF;
+    }
+
+    public function other() {
+        return <<<EOF
+        <html>
+        <body>
+        <p>これはHelloコントローラのOhterアクションです</p>
+        </body>
+        </html>
+        EOF;
+    }
 }
