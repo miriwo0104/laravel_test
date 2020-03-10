4c0a067ef4f5bc00afcbdceab51512b89f0eb4fd
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-03-10 19:53:36 +0900

    ルーティングとビューのみを使用して/helloページの表示を行った
    コントローラファイルは使用していない

---
+++ resources/views/hello/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello/Index</title>
+    <style>
+    body {font-size:16pt; color:#999; }
+    h1 {font-size:100pt; text-align:right; color:#f6f6f6; margin:-50px 0px -100px 0px; }
+    </style>
+</head>
+<body>
+    <h1>Index</h1>
+    <P>This is a sample page with php-template</P>
+</body>
+</html>
